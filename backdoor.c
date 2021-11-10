b27010e564337baff2dfa0d86779baf974a5f7fd
ROHIDH <87977972+CodeCRAFT-rk@users.noreply.github.com>
2021-11-10 21:28:53 +0530

    Update backdoor.c

--- backdoor.c
+++ backdoor.c
@@ -1,36 +1,3 @@
-
-/* 
- * compile this file in windows os
- * if you encounder any error like this => {
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x5c5): undefined reference to `send@16'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x620): undefined reference to `send@16'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x6ab): undefined reference to `send@16'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x6ef): undefined reference to `send@16'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x790): undefined reference to `recv@16'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x7c2): undefined reference to `closesocket@4'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x7ca): undefined reference to `WSACleanup@0'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x92d): undefined reference to `send@16'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x9a1): undefined reference to `WSAStartup@8'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0x9d0): undefined reference to `socket@12'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0xa04): undefined reference to `inet_addr@4'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0xa16): undefined reference to `htons@4'
-    C:\Users\USER\AppData\Local\Temp\ccc67Kmi.o:backdoor.c:(.text+0xa3c): undefined reference to `connect@12'
- * }
- * you can use the following command in order to compile it in windows os
- * gcc <filename.c> -o <outputFileName.exe> -lws2_32
- */
-
-
-/* 
- * if don't have windows then run the following commands in order to compile it in any linux os
- * sudo apt update
- * sudo apt install mingw-w64
- * i686-w64-mingw32-gcc -o backdoor.exe backdoor.c -lwininet -lwsock32
- * 
- * here -lwininet and -lwsock32 are the libraries that we are used extra that linux don't have in default
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
